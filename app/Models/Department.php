9b85a9bd53c5a30f005860eb35915a56161c5ccf
pimphand <faisaldwiki69@gmail.com>
2024-09-03 11:12:14 +0700

    add softdeletes

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Multitenant;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Department extends Model
 {
-    use HasFactory, Multitenant;
+    use HasFactory, Multitenant, SoftDeletes;
 
-    protected $fillable=['name'];
+    protected $fillable = ['name'];
 }
